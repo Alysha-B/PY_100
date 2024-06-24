11450e134b0752dc23bea35bd65eb874d4acaf33
Alysha Braithwaite <alyshabraithwaite@gmail.com>
2024-06-24 16:22:54 +0000

    Added exercises fromData Types chapter

---
+++ data_types.py/2.py
@@ -0,0 +1,11 @@
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+print(names)
+
+my_pet_names = (
+    'Brandi',
+    'Sally',
+    'Daisy',
+    'Lulu',
+    'Kai',
+)
+print(my_pet_names)
---
+++ data_types.py/3.py
@@ -0,0 +1,11 @@
+pets = {'Asta': 'dog', 'Butterscotch': 'cat', 'Pudding': 'cat', 'Neptune': 'fish', 'Darwin': 'lizard'}
+print(pets)
+
+my_pets = {
+    'Brandi': 'dog',
+    'Sally': 'cat',
+    'Daisy': 'dog',
+    'Lulu': 'dog',
+    'Kai': 'dog',
+}
+print(my_pets)
